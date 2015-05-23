d1cd16c7c07c31e903483ee373e59f5a4eed2213
SismoMundial <Sismologia.Mundial@gmail.com>
2015-05-23 17:17:57 -0400

    got
    Go

---
+++ app/controllers/blog_articles_controller.rb
@@ -0,0 +1,8 @@
+class BlogArticlesController < ApplicationController
+  def feed
+    @blog_articles = BlogArticle.all
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+  end
+end
